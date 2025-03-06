69834a68061e7b98c2a6d1538f498b1addce6829
Michael-Mantovani <m.mantovani@esis-italia.com>
2025-03-06 11:56:39 +0100

    Add deleteById test

--- src/test/java/com/example/supermarket/service/ProductServiceTest.java
+++ src/test/java/com/example/supermarket/service/ProductServiceTest.java
@@ -2,12 +2,14 @@ package com.example.supermarket.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,10 +25,39 @@ public class ProductServiceTest {
 
     @Test
     void testDeleteAll() {
+        // GIVEN
+        List<Product> products = List.of(
+                new Product(1, "Nome", 12, null, new HashSet<>()),
+                new Product(2, "Nome", 15, null, new HashSet<>()));
+
+        // WHEN
+        when(productService.findAll()).thenReturn(products);
+        productService.deleteAll();
+        verify(productService, times(1)).deleteAll();
+        when(productService.findAll()).thenReturn(null);
+
+        // VERIFY
+        List<Product> deletedProducts = productService.findAll();
+        assertNull(deletedProducts);
+
     }
 
     @Test
     void testDeleteById() {
+
+        // GIVEN
+        int id = 1;
+        Product product = new Product(id, "Nome", 12, null, new HashSet<>());
+
+        // WHEN
+        when(productService.findById(id)).thenReturn(Optional.of(product));
+        productService.deleteById(id);
+        verify(productService, times(1)).deleteById(id);
+        when(productService.findById(id)).thenReturn(null);
+
+        // VERIFY
+        Optional<Product> deletedProduct = productService.findById(id);
+        assertNull(deletedProduct);
 
     }
 
