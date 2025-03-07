460b123a30a9a52939bc91304459cd33b1e05568
Michael-Mantovani <m.mantovani@esis-italia.com>
2025-03-07 15:54:34 +0100

    Correct createNewCategory

--- src/main/java/com/example/supermarket/controller/CategoryController.java
+++ src/main/java/com/example/supermarket/controller/CategoryController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @Transactional
@@ -28,7 +29,7 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    @GetMapping("/add")
+    @PostMapping("/add")
     public ResponseEntity<?> createNewCategory(@Valid @ModelAttribute Category category) {
 
         try {
