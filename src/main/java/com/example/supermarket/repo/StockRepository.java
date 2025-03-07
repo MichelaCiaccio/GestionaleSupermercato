2cc306ae50c6969ebec1f7f33631fbec68460610
Michael-Mantovani <m.mantovani@esis-italia.com>
2025-03-07 17:58:51 +0100

    Update StockRepository

--- src/main/java/com/example/supermarket/repo/StockRepository.java
+++ src/main/java/com/example/supermarket/repo/StockRepository.java
@@ -1,13 +1,15 @@
 package com.example.supermarket.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.supermarket.entity.Stock;
 import java.util.List;
 import com.example.supermarket.entity.Product;
 import com.example.supermarket.entity.Supplier;
 
-public interface StockService extends JpaRepository<Stock, Integer> {
+@Repository
+public interface StockRepository extends JpaRepository<Stock, Integer> {
 
     List<Stock> findByProduct(Product product);
 
