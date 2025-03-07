b901bc30cddd2c2f81cc934fc6e100d6ee3404ec
Michael-Mantovani <m.mantovani@esis-italia.com>
2025-03-07 15:39:20 +0100

    Add Service annotation to CategoryService

--- src/main/java/com/example/supermarket/service/CategoryService.java
+++ src/main/java/com/example/supermarket/service/CategoryService.java
@@ -3,12 +3,14 @@ package com.example.supermarket.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.supermarket.entity.Category;
 import com.example.supermarket.repo.CategoryRepository;
 
 import jakarta.persistence.EntityNotFoundException;
 
+@Service
 public class CategoryService {
 
     @Autowired
