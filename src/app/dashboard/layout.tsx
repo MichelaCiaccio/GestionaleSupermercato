2bcae7b87b50ca9bd613a24dc0696fcf574bbce9
Sherwin Arellano <s.arellano@esis-italia.com>
2025-03-11 10:36:25 +0100

    Fix dashboard overview title

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,7 @@
 import { AppSidebar } from '@/components/ui/dashboard/app-sidebar';
 import Header from '@/components/ui/dashboard/header';
 import { SidebarProvider } from '@/components/ui/sidebar';
-import { Metadata } from 'next';
 import { ReactNode } from 'react';
-
-export const metadata: Metadata = {
-  title: 'Dashboard',
-};
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
