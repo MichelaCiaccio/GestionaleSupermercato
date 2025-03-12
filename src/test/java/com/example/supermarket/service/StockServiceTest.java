23fad70c55afea8a2dc695d6e7abfffea3c1b040
Michael-Mantovani <m.mantovani@esis-italia.com>
2025-03-12 15:25:42 +0100

    Add findByExpirationDateBetween test in StockServiceTest

--- src/test/java/com/example/supermarket/service/StockServiceTest.java
+++ src/test/java/com/example/supermarket/service/StockServiceTest.java
@@ -219,6 +219,41 @@ public class StockServiceTest {
     @Test
     void testFindByExpirationDateBetween() {
 
+        // GIVEN
+        LocalDate expirationDateStart = LocalDate.of(2025, 3, 12);
+        LocalDate expirationDateEnd = LocalDate.of(2026, 3, 12);
+        Product product = new Product(1, "Prodotto", new BigDecimal(26), null, null);
+        Supplier supplier = new Supplier(1, "Fornitore", "Indirizzo", "3336875889", "test@email.com", null);
+        List<Stock> stocks = List.of(
+                new Stock(1, 36, expirationDateStart, LocalDate.of(2026, 02, 15), product, supplier),
+                new Stock(2, 36, expirationDateEnd, LocalDate.of(2026, 02, 15), product, supplier));
+
+        // WHEN
+        when(stockRepo.findByExpirationDateBetween(expirationDateStart, expirationDateEnd, null)).thenReturn(stocks);
+        List<Stock> ret = stockRepo.findByExpirationDateBetween(expirationDateStart, expirationDateEnd, null);
+
+        // VERIFY
+        verify(stockRepo, times(1)).findByExpirationDateBetween(expirationDateStart, expirationDateEnd, null);
+        assertEquals(ret, stocks);
+        assertEquals(2, ret.size());
+        assertNotNull(ret);
+    }
+
+    @Test
+    void findByExpirationDateBetweenException() {
+
+        // GIVEN
+        LocalDate expirationDateStart = LocalDate.of(2025, 3, 12);
+        LocalDate expirationDateEnd = LocalDate.of(2026, 3, 12);
+
+        // WHEN
+        when(stockRepo.findByExpirationDateBetween(expirationDateStart, expirationDateEnd, null))
+                .thenThrow(new EntityNotFoundException());
+
+        // VERIFY
+        verify(stockRepo, times(0)).findByExpirationDateBetween(expirationDateStart, expirationDateEnd, null);
+        assertThrows(EntityNotFoundException.class,
+                () -> stockRepo.findByExpirationDateBetween(expirationDateStart, expirationDateEnd, null));
     }
 
     @Test
