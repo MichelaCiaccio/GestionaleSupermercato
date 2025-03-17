55bb6d64bbf2344de79f24b55952f5e0c84a2aa5
Sherwin Arellano <s.arellano@esis-italia.com>
2025-03-17 10:31:28 +0100

    Output api request/response for debugging

--- src/lib/db/instance.ts
+++ src/lib/db/instance.ts
@@ -1,7 +1,22 @@
+import { env } from '@/data/env/server';
 import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'http://localhost:8080/',
 });
+
+if (env.NODE_ENV === 'development') {
+  instance.interceptors.request.use((request) => {
+    console.log(
+      `[DEV] Request: ${request.method?.toUpperCase()} ${request.url}`
+    );
+    return request;
+  });
+
+  instance.interceptors.response.use((response) => {
+    console.log(`[DEV] Response (${response.config.url}):`, response.data);
+    return response;
+  });
+}
 
 export default instance;
