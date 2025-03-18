502d5a3585285117b6660ef5d6fc3ed3905caec2
Michael-Mantovani <m.mantovani@esis-italia.com>
2025-03-18 16:21:15 +0100

    Update ProductRepository and StockRepository

--- src/main/java/com/example/supermarket/repo/ProductRepository.java
+++ src/main/java/com/example/supermarket/repo/ProductRepository.java
@@ -1,5 +1,9 @@
 package com.example.supermarket.repo;
 
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -17,7 +21,13 @@ import java.util.List;
 @Transactional
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-    Optional<Product> findById(Integer id);
+    @Override
+    @Nonnull
+    Optional<Product> findById(@Nullable Integer id);
+
+    @Override
+    @Nonnull
+    Page<Product> findAll(@Nullable  Pageable pageable);
 
     List<Product> findByName(String name);
 
