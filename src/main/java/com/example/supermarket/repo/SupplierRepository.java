f36d43739d5ff7c189e2b4428756dc36b90a1f21
Michael-Mantovani <m.mantovani@esis-italia.com>
2025-03-20 16:32:52 +0100

    Update SupplierService

--- src/main/java/com/example/supermarket/repo/SupplierRepository.java
+++ src/main/java/com/example/supermarket/repo/SupplierRepository.java
@@ -1,13 +1,12 @@
 package com.example.supermarket.repo;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.example.supermarket.entity.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.supermarket.entity.Supplier;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @Transactional
@@ -15,5 +14,5 @@ public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
 
     Optional<Supplier> findByName(String name);
 
-    List<Supplier> findByStocksProductName(String name);
+    List<Supplier> findByProducts_Name(String ProductName);
 }
--- src/main/java/com/example/supermarket/service/SupplierService.java
+++ src/main/java/com/example/supermarket/service/SupplierService.java
@@ -16,41 +16,50 @@ public class SupplierService {
     private SupplierRepository supplierRepo;
 
     /**
-     * This method searches for supplier using the id as a parameter.
-     * If the supplier doesn't exists throw an EntityNotFoundException.
+     * This method searches for a supplier by its ID.
+     * If the supplier does not exist, throw an EntityNotFoundException.
+     * Otherwise, return the found supplier;
      *
-     * @param id the id of the supplier
-     * @return the supplier found
+     * @param id The ID of the supplier
+     * @return The supplier found
      */
     public Supplier findById(int id) {
         return supplierRepo.findById(id).orElseThrow(() -> new EntityNotFoundException("Supplier with id " + id + " not found"));
     }
 
     /**
-     * This method searches for supplier using the name as a parameter.
-     * If the supplier doesn't exists throw an EntityNotFoundException.
+     * This method searches for supplier by its name.
+     * If the supplier does not exist, throw an EntityNotFoundException.
+     * Otherwise, return the found supplier.
      *
-     * @param name the name of the supplier
-     * @return the supplier found
+     * @param name The name of the supplier
+     * @return The supplier found
      */
     public Supplier findByName(String name) {
-        Supplier supplier = supplierRepo.findByName(name)
+        return supplierRepo.findByName(name)
                 .orElseThrow(() -> new EntityNotFoundException("Supplier with name " + name + " not found"));
-        return supplier;
     }
 
-    public List<Supplier> findByProductName(String name) {
-        List<Supplier> suppliers = supplierRepo.findByStocksProductName(name);
+    /**
+     * This method searches for suppliers who supply a product identified by its name.
+     * If no suppliers are found, throw an EntityNotFoundException.
+     * Otherwise, returns the list of found suppliers
+     *
+     * @param productName The name of the product
+     * @return A list of found suppliers.
+     */
+    public List<Supplier> findByProductName(String productName) {
+        List<Supplier> suppliers = supplierRepo.findByProducts_Name(productName);
         if (suppliers.isEmpty()) {
-            throw new EntityNotFoundException("Suppliers providing the product " + name + " not found");
+            throw new EntityNotFoundException("Suppliers providing the product " + productName + " not found");
         }
         return suppliers;
     }
 
     /**
-     * This method delete all the suppliers.
-     * If there are no suppliers it throws an EntityNotFoundException.
-     * Otherwise calls supplierRepository.deleteAll to delete all the suppliers.
+     * This method deletes all the suppliers.
+     * If there are no suppliers, it throws an EntityNotFoundException.
+     * Otherwise, call supplierRepository.deleteAll to delete all the suppliers.
      */
     public void deleteAll() {
         if (supplierRepo.findAll().isEmpty()) {
@@ -63,9 +72,9 @@ public class SupplierService {
      * This method deletes a supplier identified by its id.
      * Check if the supplier exists and, if it does, proceed to call
      * supplierRepository.deleteById to delete it.
-     * Otherwise it throws an EntityNotFoundException.
+     * Otherwise, it throws an EntityNotFoundException.
      *
-     * @param id
+     * @param id The id of the supplier
      */
     public void deleteById(int id) {
         if (supplierRepo.findById(id).isEmpty()) {
@@ -77,12 +86,10 @@ public class SupplierService {
     /**
      * This method creates a new supplier.
      * It checks if the supplier already exists, searching by its name, and if it
-     * does, throws a
-     * DuplicateKeyException.
-     * Otherwise calls the supplierRepository.save method to create the new
-     * supplier.
+     * does, throws a DuplicateKeyException.
+     * Otherwise, proceed to create the new supplier.
      *
-     * @param supplier
+     * @param supplier The supplier to be saved
      */
     public void save(Supplier supplier) {
         if (supplierRepo.findByName(supplier.getName()).isPresent()) {
@@ -111,6 +118,7 @@ public class SupplierService {
         supplier.setPhoneNumber(modSupplier.getPhoneNumber());
         supplier.setStocks(modSupplier.getStocks());
         supplier.setAddress(modSupplier.getAddress());
+        supplier.setProducts(modSupplier.getProducts());
         supplierRepo.save(supplier);
     }
 }
