10a5d69bc3eed02b9d208dca4c4242f68d848ebd
Sherwin Arellano <s.arellano@esis-italia.com>
2025-03-21 15:19:04 +0100

    Configure auth jwt and session to include user

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,4 +1,23 @@
-import type { NextAuthConfig } from 'next-auth';
+import 'next-auth/jwt';
+import type { DefaultSession, NextAuthConfig } from 'next-auth';
+import { User as IUser } from './types/entities/user';
+
+type AuthUser = Omit<IUser, 'password'>;
+
+declare module 'next-auth' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface User extends AuthUser {}
+
+  interface Session {
+    user: AuthUser & DefaultSession['user'];
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    user?: AuthUser;
+  }
+}
 
 export const authConfig = {
   pages: {
@@ -15,6 +34,19 @@ export const authConfig = {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
+    },
+    jwt({ token, user }) {
+      if (user) {
+        delete (user as Partial<IUser>).password;
+        token.user = user as typeof token.user;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      if (token.user) {
+        Object.assign(session.user, token.user);
+      }
+      return session;
     },
   },
   providers: [], // Add providers with an empty array for now
