c094e0fb8b6d9b32798b3dcef0ebb1f73fe880a0
Michael-Mantovani <m.mantovani@esis-italia.com>
2025-03-28 12:27:41 +0100

    Add findAll method to SaleService

--- src/main/java/com/example/supermarket/repo/SaleRepository.java
+++ src/main/java/com/example/supermarket/repo/SaleRepository.java
@@ -3,17 +3,26 @@ package com.example.supermarket.repo;
 import com.example.supermarket.entity.Sale;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
+@Transactional
 public interface SaleRepository extends JpaRepository<Sale, Integer> {
 
     @Override
     @Nonnull
     Optional<Sale> findById(@Nullable Integer id);
+
+    @Nonnull
+    Page<Sale> findAll(@Nullable Pageable pageable);
 
     List<Sale> findByProductSales_Product_Name(String productSale);
 
---
+++ src/main/java/com/example/supermarket/service/SaleService.java
@@ -0,0 +1,49 @@
+package com.example.supermarket.service;
+
+import com.example.supermarket.entity.Sale;
+import com.example.supermarket.repo.SaleRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository saleRepo;
+
+    /**
+     * This method searches for all the sales, organizes them into pagination of 20 elements,
+     * and sorts them according
+     * to a specified direction.
+     * If the client does not provide the page number or sort direction,
+     * default values are set.
+     * Check if any sale exists and return them.
+     * Otherwise, it throws and EntityNotFoundException
+     *
+     * @param page          The page number the client wants to display.
+     * @param sortDirection The direction in which the client wants the sales to be ordered.
+     *                      Defaults to "ASC" if null or blank.
+     * @return A Page containing the list of sales.
+     */
+    public Page<Sale> findAllSalesSorted(Integer page, String sortDirection) {
+        page = page == null ? 0 : page;
+
+        sortDirection = sortDirection == null || sortDirection.isBlank() ? "ASC" : sortDirection;
+
+
+        Sort.Direction direction = Sort.Direction.fromString(sortDirection.toUpperCase());
+        Pageable pageable = PageRequest.of(page, 20, Sort.by(direction));
+        Page<Sale> sales = saleRepo.findAll(pageable);
+        if (sales.isEmpty()) {
+            throw new EntityNotFoundException("There are no registered sales");
+        }
+
+        return sales;
+
+    }
+}
