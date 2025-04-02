b5e9835005f5be1caac20082beb8b571e9335e3f
Michael-Mantovani <m.mantovani@esis-italia.com>
2025-04-02 15:18:05 +0200

    Add another DealType

--- src/main/java/com/example/supermarket/entity/enums/DealType.java
+++ src/main/java/com/example/supermarket/entity/enums/DealType.java
@@ -2,6 +2,10 @@ package com.example.supermarket.entity.enums;
 
 public enum DealType {
 
+    // Prende 3, paga 2
     THREEPAYTWO,
+
+    // Sconto su una spesa di prodotto oltre un certo prezzo totale
+    DISCOUNTONTOTAL,
 
 }
--- src/main/java/com/example/supermarket/service/DealService.java
+++ src/main/java/com/example/supermarket/service/DealService.java
@@ -50,4 +50,6 @@ public class DealService {
         }
         return deals;
     }
+
+    
 }
