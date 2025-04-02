f2f7610c4b730ee071cd7a2057b2405fae397a3d
Michael-Mantovani <m.mantovani@esis-italia.com>
2025-04-02 14:39:49 +0200

    Add DealRepository

---
+++ src/main/java/com/example/supermarket/repo/DealRepository.java
@@ -0,0 +1,9 @@
+package com.example.supermarket.repo;
+
+import com.example.supermarket.entity.Deal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DealRepository extends JpaRepository<Deal, Integer> {
+}
