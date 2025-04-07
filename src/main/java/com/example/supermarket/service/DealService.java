fe2d77fc08f371ca5df4a3d6f75173a515c5b8d1
Michael-Mantovani <m.mantovani@esis-italia.com>
2025-04-07 12:40:40 +0200

    Update DealService

--- src/main/java/com/example/supermarket/service/DealService.java
+++ src/main/java/com/example/supermarket/service/DealService.java
@@ -1,10 +1,6 @@
 package com.example.supermarket.service;
 
-import com.example.supermarket.entity.Category;
 import com.example.supermarket.entity.Deal;
-import com.example.supermarket.entity.Product;
-import com.example.supermarket.entity.ProductSale;
-import com.example.supermarket.entity.enums.DealType;
 import com.example.supermarket.repo.DealRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
@@ -16,9 +12,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -81,47 +75,5 @@ public class DealService {
             }
             dealRepo.save(deal);
         }
-    }
-
-    /**
-     * This method applies the BUY3PAY2 deal.
-     * It selects the products that belong to the categories associated with the deal,
-     * calculates the number of valid groups of 3 products,
-     * and returns the free products from those groups.
-     * If the deal type is BUY3PAY2 (THREEPAYTWO), and there are at least 3 products eligible
-     * for the promotion, it will calculate how many groups of 3 products can be formed. The
-     * least expensive products from each group will be selected and returned.
-     *
-     * @param productSales A list of ProductSale objects representing the products sold in the sale.
-     * @param deal         The promotion deal to be applied, which contains the promotion type
-     *                     and categories.
-     * @return A list of the least expensive products from the valid groups of 3 eligible products
-     * based on the promotion.
-     */
-    public BigDecimal applyBUY3PAY2Deal(List<ProductSale> productSales, Deal deal) {
-
-        if (deal == null) {
-            return BigDecimal.ZERO;
-        }
-        // Recuperare la categoria a cui fa riferimento la promozione
-        List<Category> targetCategories = deal.getCategory();
-
-        // Recupero i prodotti che hanno la stessa categoria di quelle in promozione
-        List<Product> targetProducts =
-                productSales.stream().map(ProductSale::getProduct).filter(product -> targetCategories.stream()
-                        .anyMatch(category -> category.equals(product.getCategory()))).toList();
-
-        // Se il deal è THREEPAYTWO e i prodotti validi sono 3 o più, conto quanti gruppi da 3
-        // prodotti esistono
-        int groupProducts =
-                (deal.getDealType() == DealType.BUY3PAY2 && targetProducts.size() >= 3) ?
-                        targetProducts.size() / 3 : 0;
-
-        List<Product> freeProducts =
-                targetProducts.stream().sorted(Comparator.comparing(Product::getSellingPrice)).limit(groupProducts).toList();
-
-        // Restituisco i prodotti meno cari in basi al numero di gruppi da 3
-        return freeProducts.stream().map(Product::getSellingPrice).reduce(BigDecimal.ZERO,
-                                                                          BigDecimal::add);
     }
 }
