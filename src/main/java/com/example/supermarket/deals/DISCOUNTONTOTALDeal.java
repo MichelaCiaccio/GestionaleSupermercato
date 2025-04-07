23d4604485e71eaf3ff53eccd43390702107a26c
Michael-Mantovani <m.mantovani@esis-italia.com>
2025-04-07 14:54:41 +0200

    Add DISCOUNTONTOTALDeal

--- src/main/java/com/example/supermarket/deals/DISCOUNTONTOTALDeal.java
+++ src/main/java/com/example/supermarket/deals/DISCOUNTONTOTALDeal.java
@@ -1,6 +1,8 @@
 package com.example.supermarket.deals;
 
+import com.example.supermarket.entity.Category;
 import com.example.supermarket.entity.Deal;
+import com.example.supermarket.entity.Product;
 import com.example.supermarket.entity.ProductSale;
 
 import java.math.BigDecimal;
@@ -9,6 +11,24 @@ import java.util.List;
 public class DISCOUNTONTOTALDeal implements DealStrategy {
     @Override
     public BigDecimal applyDeal(List<ProductSale> productSales, Deal deal) {
-        return null;
+
+        // Recuperare la categoria a cui fa riferimento la promozione
+        List<Category> targetCategories = deal.getCategory();
+
+        // Recupero i prodotti che hanno la stessa categoria di quelle in promozione
+        List<Product> targetProducts =
+                productSales.stream().map(ProductSale::getProduct).filter(product -> targetCategories.stream()
+                        .anyMatch(category -> category.equals(product.getCategory()))).toList();
+
+        BigDecimal totalPrice =
+                targetProducts.stream().map(Product::getSellingPrice).reduce(BigDecimal.ZERO,
+                                                                             BigDecimal::add);
+        BigDecimal discountedPrice =
+                targetProducts.stream().map(Product::getSellingPrice).reduce(BigDecimal.ZERO,
+                                                                             BigDecimal::add).multiply(BigDecimal.valueOf(0.2));
+
+        return totalPrice.compareTo(BigDecimal.valueOf(50)) >= 0 ?
+                totalPrice.subtract(discountedPrice) : BigDecimal.ZERO;
+
     }
 }
