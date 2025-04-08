8627b7042c88547f6786a23628fbf5544c51cd41
Michael-Mantovani <m.mantovani@esis-italia.com>
2025-04-08 16:25:41 +0200

    Delete getBestSellingProducts test in SaleServiceTest

--- src/test/java/com/example/supermarket/service/SaleServiceTest.java
+++ src/test/java/com/example/supermarket/service/SaleServiceTest.java
@@ -379,47 +379,5 @@ class SaleServiceTest {
         assertEquals(BigDecimal.valueOf(100), totalAmount);
     }
 
-    @Test
-    void getBestSellingProducts() {
-
-        // Given
-        Product product1 = new Product(1, "Product 1", BigDecimal.valueOf(10),
-                                       BigDecimal.valueOf(10), false, null,
-                                       null,
-                                       null);
-        Product product2 = new Product(2, "Product 2", BigDecimal.valueOf(20),
-                                       BigDecimal.valueOf(20), false, null,
-                                       null,
-                                       null);
-        Product product3 = new Product(3, "Product 3", BigDecimal.valueOf(30),
-                                       BigDecimal.valueOf(30), false, null,
-                                       null,
-                                       null);
-
-        ProductSale productSale1 = new ProductSale(1, 5, product1, null);
-        ProductSale productSale2 = new ProductSale(2, 15, product2, null);
-        ProductSale productSale3 = new ProductSale(3, 25, product3, null);
-
-        Sale sale1 = new Sale(1, BigDecimal.valueOf(100), BigDecimal.valueOf(100),
-                              LocalDateTime.now(), List.of(productSale1, productSale2), null);
-        Sale sale2 = new Sale(2, BigDecimal.valueOf(100), BigDecimal.valueOf(100),
-                              LocalDateTime.now(), List.of(productSale3), null);
-
-        LocalDate startDate = LocalDate.now().minusDays(1);
-        LocalDate endDate = LocalDate.now().plusDays(1);
-
-        // When
-        when(saleServ.findBetweenDate(startDate, endDate)).thenReturn(List.of(sale1, sale2));
-        List<Product> bestSellingProducts =
-                saleServ.getBestSellingProducts(startDate, endDate);
-
-        // Verify
-        verify(saleRepo, times(1)).findBySaleDateBetween(any(LocalDateTime.class),
-                                                         any(LocalDateTime.class));
-        assertEquals(3, bestSellingProducts.size()); // Verifica che siano 3 prodotti
-        assertEquals(product3, bestSellingProducts.get(0)); // Il prodotto più venduto
-        assertEquals(product2, bestSellingProducts.get(1)); // Il secondo più venduto
-        assertEquals(product1, bestSellingProducts.get(2)); // Il terzo più venduto
-
-    }
+   
 }
