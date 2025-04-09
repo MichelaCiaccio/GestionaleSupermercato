cab8ac92b7f400855a26ef8dfd8fa5eb82d1c2c0
Michael-Mantovani <m.mantovani@esis-italia.com>
2025-04-09 17:30:12 +0200

    Add getStocksRecords in StocksReportService

--- src/main/java/com/example/supermarket/service/StocksReportService.java
+++ src/main/java/com/example/supermarket/service/StocksReportService.java
@@ -1,5 +1,7 @@
 package com.example.supermarket.service;
 
+import com.example.supermarket.DTO.ProductDTO;
+import com.example.supermarket.DTO.Report.StocksRecord;
 import com.example.supermarket.entity.Stock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,9 @@ public class StocksReportService {
 
     @Autowired
     private StockService stockServ;
+
+    @Autowired
+    private ProductService productServ;
 
     /**
      * Calculates the total quantity of all items in the warehouse.
@@ -25,5 +30,24 @@ public class StocksReportService {
 
         // Recupero le quantità e le sommo
         return stocks.stream().mapToInt(Stock::getQuantity).sum();
+    }
+
+
+    /**
+     * This method retrieves the list of all stock entries and maps them to StocksRecord.
+     *
+     * @return a list of StocksRecord
+     */
+    public List<StocksRecord> getStocksRecords() {
+
+        // Recupero tutti gli stock
+        List<Stock> stocks = stockServ.findAll();
+
+        // Recupero le informazioni dallo stock e creo i record per ciascun prodotto
+        return stocks.stream().map(stock -> {
+            ProductDTO product = productServ.findById(stock.getProduct().getId());
+            return new StocksRecord(product, stock.getExpirationDate(), stock.getQuantity());
+        }).toList();
+
     }
 }
