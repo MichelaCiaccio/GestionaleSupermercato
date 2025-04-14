47b5c45eec96e00d0cf1eb06cb6fe358a0ce5365
Michael-Mantovani <m.mantovani@esis-italia.com>
2025-04-14 11:55:53 +0200

    Update ReceiptMapper

--- src/main/java/com/example/supermarket/DTO/Mapper/ReceiptMapper.java
+++ src/main/java/com/example/supermarket/DTO/Mapper/ReceiptMapper.java
@@ -1,20 +1,12 @@
 package com.example.supermarket.DTO.Mapper;
 
 import com.example.supermarket.DTO.ReceiptDTO;
-import com.example.supermarket.DTO.SaleDTO;
 import com.example.supermarket.entity.Receipt;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-@Component
-public class ReceiptMapper {
+@Mapper(componentModel = "spring", uses = SaleMapper.class)
+public interface ReceiptMapper {
 
-    @Autowired
-    private SaleMapper saleMapper;
 
-    public ReceiptDTO toReceiptDTO(Receipt receipt) {
-        SaleDTO saleDTO = saleMapper.toSaleDto(receipt.getSale());
-
-        return new ReceiptDTO(receipt.getReceiptCode(), saleDTO);
-    }
+    ReceiptDTO toReceiptDTO(Receipt receipt);
 }
