fc5b37f39f98e3595d57f4310591dd92fd861c4c
Michael-Mantovani <m.mantovani@esis-italia.com>
2025-04-14 11:34:47 +0200

    Update StockMapper

--- src/main/java/com/example/supermarket/DTO/Mapper/StockMapper.java
+++ src/main/java/com/example/supermarket/DTO/Mapper/StockMapper.java
@@ -1,20 +1,11 @@
 package com.example.supermarket.DTO.Mapper;
 
 import com.example.supermarket.DTO.StockDTO;
-import com.example.supermarket.DTO.SupplierDTO;
 import com.example.supermarket.entity.Stock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
-@Component
-public class StockMapper {
+@Mapper(componentModel = "spring", uses = SupplierMapper.class)
+public interface StockMapper {
 
-    @Autowired
-    private SupplierMapper supplierMapper;
-
-    public StockDTO toStockDTO(Stock stock) {
-        SupplierDTO supplierDTO = supplierMapper.toSupplierDTO(stock.getSupplier());
-        return new StockDTO(stock.getQuantity(), stock.getDeliveryDate(),
-                            stock.getExpirationDate(), supplierDTO);
-    }
+    StockDTO toStockDTO(Stock stock);
 }
